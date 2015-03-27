35bb7003b209ce1678ba674d12159cc32b8c6263
BeaulieuLoic <beaulielloic@gmail.com>
2015-03-27 10:31:59 +0100

    mise a jour main activity

--- Mini-Projet-Android/src/com/example/mini_projet_android/MainActivity.java
+++ Mini-Projet-Android/src/com/example/mini_projet_android/MainActivity.java
@@ -1,16 +1,16 @@
 package com.example.mini_projet_android;
 
-
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
-import android.widget.TextView;
 
-public class MainActivity extends Activity {
+
+public class MainActivity extends Activity implements OnClickListener{
 	private Spinner nbrRecherche;
 	private Button rechercher;
 	private EditText zoneSaisie;
@@ -23,10 +23,21 @@ public class MainActivity extends Activity {
 		
 		
 		zoneSaisie = (EditText)findViewById(R.id.zoneSaisie);
+		
 		rechercher = (Button)findViewById(R.id.rechercher);
+		rechercher.setOnClickListener(this);
+		
 		nbrRecherche = (Spinner)findViewById(R.id.nbrRecherche);
 		
 		
 		
+	}
+
+
+	@Override
+	public void onClick(View arg0) {
+		if(arg0.equals(rechercher)){
+
+		}		
 	}
 }
