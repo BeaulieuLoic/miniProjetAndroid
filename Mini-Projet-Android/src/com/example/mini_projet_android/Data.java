26914feb5a9c91a8f8a8527f44794fdc82dc37a2
BeaulieuLoic <beaulielloic@gmail.com>
2015-03-27 12:54:11 +0100

    update

---
+++ Mini-Projet-Android/src/com/example/mini_projet_android/Data.java
@@ -0,0 +1,8 @@
+package com.example.mini_projet_android;
+
+public interface Data {
+	//échange d'information entre activité
+	public final static String urlImage = "urlImage";
+
+	
+}
---
+++ Mini-Projet-Android/src/com/example/mini_projet_android/ShowImageActivity.java
@@ -0,0 +1,24 @@
+package com.example.mini_projet_android;
+
+import com.squareup.picasso.Picasso;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class ShowImageActivity extends Activity{
+	
+	ImageView image;
+	
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.show_image);
+		
+		String imageUrl =  getIntent().getStringExtra(Data.urlImage);
+		image = (ImageView) findViewById(R.id.imageView1);
+		Picasso.with(this).load(imageUrl).into(image);
+		
+	}
+	
+}
