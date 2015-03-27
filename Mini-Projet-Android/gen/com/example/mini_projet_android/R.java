fa9796e3e84874e44bf78c7b758ce1a723b09e52
BeaulieuLoic <beaulielloic@gmail.com>
2015-03-27 10:53:32 +0100

    mise a jour main activity

--- Mini-Projet-Android/gen/com/example/mini_projet_android/R.java
+++ Mini-Projet-Android/gen/com/example/mini_projet_android/R.java
@@ -8,16 +8,19 @@
 package com.example.mini_projet_android;
 
 public final class R {
+    public static final class array {
+        public static final int nbrRechercheListe=0x7f050000;
+    }
     public static final class attr {
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int nbrRecherche=0x7f060000;
-        public static final int rechercher=0x7f060002;
-        public static final int spinner1=0x7f060003;
-        public static final int zoneSaisie=0x7f060001;
+        public static final int nbrRecherche=0x7f070000;
+        public static final int rechercher=0x7f070003;
+        public static final int zoneSaisie=0x7f070001;
+        public static final int zoneTexte=0x7f070002;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
@@ -48,10 +51,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f050000;
+        public static final int AppBaseTheme=0x7f060000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f050001;
+        public static final int AppTheme=0x7f060001;
     }
 }
--- Mini-Projet-Android/src/com/example/mini_projet_android/MainActivity.java
+++ Mini-Projet-Android/src/com/example/mini_projet_android/MainActivity.java
@@ -1,19 +1,32 @@
 package com.example.mini_projet_android;
 
+import org.apache.http.Header;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity implements OnClickListener{
 	private Spinner nbrRecherche;
 	private Button rechercher;
 	private EditText zoneSaisie;
+	
+	private MainActivity act = this;
+
 	
 	
 	@Override
@@ -39,5 +52,35 @@ public class MainActivity extends Activity implements OnClickListener{
 		if(arg0.equals(rechercher)){
 
 		}		
+	}
+	
+	
+	public void lancerGet(String url){
+		AsyncHttpClient client = new AsyncHttpClient();
+		client.get(url, new AsyncHttpResponseHandler(){
+			@Override
+			public void onFailure(int arg0, Header[] arg1, byte[] arg2,
+					Throwable arg3) {
+				Log.e("erreur json",arg3.getMessage());
+				
+			}
+
+			@Override
+			public void onSuccess(int arg0, Header[] arg1, byte[] arg2) {
+				String s = "";
+				String jsonData = new String(arg2);
+				
+				
+				try {
+					JSONObject repObj = (JSONObject) new JSONTokener(jsonData).nextValue();
+					Toast.makeText(act, repObj.getString("nom").toString(), Toast.LENGTH_LONG).show();
+					
+				} catch (JSONException je) {
+					Log.e("TAG", "ERREUR:"+je.getMessage() );
+				}
+				
+			}
+			
+		});
 	}
 }
