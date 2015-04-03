6c9c503835acab97b57efc8868c5b62147a240b9
BeaulieuLoic <beaulielloic@gmail.com>
2015-04-03 10:47:11 +0200

    update  xml bouton/spinner

--- Mini-Projet-Android/gen/com/example/mini_projet_android/R.java
+++ Mini-Projet-Android/gen/com/example/mini_projet_android/R.java
@@ -17,13 +17,14 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int icon=0x7f070005;
-        public static final int imageView1=0x7f070006;
-        public static final int listeView=0x7f070004;
-        public static final int nbrRecherche=0x7f070000;
+        public static final int icon=0x7f070006;
+        public static final int imageView1=0x7f070007;
+        public static final int layout=0x7f070002;
+        public static final int listeView=0x7f070005;
+        public static final int nbrRecherche=0x7f070004;
         public static final int rechercher=0x7f070003;
         public static final int zoneSaisie=0x7f070001;
-        public static final int zoneTexte=0x7f070002;
+        public static final int zoneTexte=0x7f070000;
     }
     public static final class layout {
         public static final int activity_main=0x7f030000;
